49b6b751e2329bb59ad6c9cf64a07d6300286889
Benjamin M <lolgame99@gmail.com>
2022-09-19 14:36:08 +0200

    fixed fab position & added fab group

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,27 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import { FAB } from "react-native-paper";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 
 const MainScreen = () => {
-    
+    const [state, setState] = React.useState({ open: false });
+
+    const onStateChange = ({ open }) => setState({ open });
+    const windowWidth = Dimensions.get('window').width;
+
+    const { open } = state;
+
     return (
         <View style={styles.container}>
-            <FAB
-            icon="star-outline"
-            style={styles.fab}
-            onPress={() => console.log("Pressed")}
-            size="small"
-            />
-            <FAB
-            icon="account-plus-outline"
-            style={styles.fab}
-            onPress={() => console.log("Pressed")}
-            />
-            <FAB
-            icon="wrench-outline"
-            style={styles.fab}
-            onPress={() => console.log("Pressed")}
-            size="small"
-            />
+            <View style={styles.contentContainer}></View>
+            <View style={styles.navContainer}>
+                <FAB
+                    icon="star-outline"
+                    style={[styles.fab, styles.fabLeft]}
+                    onPress={() => console.log("Pressed star")}
+                    size="small"
+                />
+                <FAB.Group
+                    style={{paddingRight: windowWidth / 2.56}}
+                    open={open}
+                    icon="account-plus-outline"
+                    actions={[
+                        {
+                            icon: 'plus',
+                            label: 'Create Friend',
+                            onPress: () => console.log('Pressed create'),
+                        },
+                        {
+                            icon: 'import',
+                            label: 'Import Friend',
+                            onPress: () => console.log('Pressed import'),
+                        },
+                    ]}
+                    onStateChange={onStateChange}
+                />
+                <FAB
+                    icon="wrench-outline"
+                    style={[styles.fab, styles.fabRight]}
+                    onPress={() => console.log("Pressed wrench")}
+                    size="small"
+                />
+            </View>
         </View>
     );
 };
@@ -29,14 +52,28 @@ const MainScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: '100%',
+        flexDirection: 'column',
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
+    },
+    contentContainer: {
+        flex: 1,
         flexDirection: 'row',
-      },
+    },
+    navContainer: {
+        width: '100%',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+    },
     fab: {
-        justifyContent: "flex-start"
+        margin: 16,
+    },
+    fabLeft: {
+        justifyContent: "flex-start",
+    },
+    fabRight: {
+        justifyContent: "flex-end",
     },
 });
 
