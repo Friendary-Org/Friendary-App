3e7bb2714c032d32fb08655b0561007c4399b0b1
Fabian Egartner <fabian.egartner@hotmail.com>
2022-10-05 14:55:50 +0200

    refactoring

--- components/ContactEntry.js
+++ components/ContactEntry.js
@@ -7,14 +7,15 @@ const ContactEntry = (props) => {
 
     const { name } = props;
     const [ checked, setChecked ] = useState("unchecked");
+    const determineCheckState = (checked == "unchecked" ? "checked" : "unchecked")
 
     return (
         <List.Item 
             title = {name} 
-            onPress={() => setChecked(checked == "unchecked"?"checked":"unchecked")}
+            onPress={() => setChecked(determineCheckState)}
             right = {() => <Checkbox 
                                 status={checked} 
-                                onPress={() => setChecked(checked == "unchecked"?"checked":"unchecked")}
+                                onPress={() => setChecked(determineCheckState)}
                             /> 
                     }
         />
