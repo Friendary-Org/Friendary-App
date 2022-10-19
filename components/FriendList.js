37649adb513fe4c8f16b9a935504855ff1c7ae2a
Fabian Egartner <fabian.egartner@hotmail.com>
2022-10-19 18:23:59 +0200

    choose active filter field

--- components/FriendList.js
+++ components/FriendList.js
@@ -6,13 +6,15 @@ import FriendEntry from './FriendEntry';
 
 const FriendList = (props) => {
 
-    const { friendList, filterString, navigation } = props;
+    const { friendList, filterString, filterType, navigation } = props;
 
     return (
         <React.Fragment>
-            {friendList.filter((friend) => friend.name !== undefined ?
+            {friendList.filter((friend) => friend.name !== undefined && filterType !== undefined && filterType.toUpperCase() === "NAMES" ?
                                            friend.name.toLowerCase().includes(filterString.toLowerCase()) :
-                                           ""                                    
+                                           friend                                    
+            ) .filter((friend) => {return friend; }
+            
             ) .map((friend) => (   
                 <React.Fragment key={friend.id}>    
                     <FriendEntry friend={friend} 
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState, useEffect} from "react";
 import { View, StyleSheet, Text, ScrollView } from "react-native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';   
+import { PaperSelect } from 'react-native-paper-select';
 
 import SearchBar from "../components/SearchBar";
 import FriendList from "../components/FriendList";
@@ -10,46 +11,112 @@ import FloatingButtonsMain from "../components/FloatingButtonsMain";
 
 const MainScreen = ({navigation}) => {
 
-    const [ friendList, setFriendList ] = useState([]);
-    const [ filterString, setFilterString ] = useState("");
-
-    // data for testing purposes
-    // const friendList = [
-    //     {id: 0, title: "Fabian Egartner", description: "Nickname: Egi2k", avatar: require('../assets/avatar.png')},
-    //     {id: 1, title: "John Walker", description: "Nickname: Cube", avatar: require('../assets/favicon.png')}
+    // let filterList = [
+    //     { _id: '1', value: "NAMES"},
+    //     { _id: '2', value: "LIKES"},
+    //     { _id: '3', value: "DISLIKES"},
+    //     { _id: '4', value: "MOVIES"},
     // ];
 
+    // const [filterList, setFilterList] = useState([{ _id: '1', value: "NAMES"}])
+    // const [filterOptions, setFilterOptions] = useState({
+    //     value: filterList[0].value,
+    //     list: filterList,
+    //     selectedList: [filterList[0]]
+    // });
+
+    // const [filterOptions, setFilterOptions] = useState({
+    //     value: filterList[0].value,
+    //     list: filterList,
+    //     selectedList: [filterList[0]]
+    // });
+
+    const [ friendList, setFriendList ] = useState([]);
+    const [ filterString, setFilterString ] = useState("");
+    const [ filterType, setFilterType ] = useState("Names");
+    const [ categoryList, setcategoryList ] = React.useState([]);
+    const [ filterOptions, setFilterOptions ] = useState(null);
+
     useEffect(() => {
-        // _removeData();
-        fetchData();
         const willFocusSubscription = navigation.addListener('focus', () => {
             fetchData();
+            fetchCategoryList();
         });
         return willFocusSubscription;
     }, []);
 
+    useEffect(() => {
+        let filterOptions = [{_id: -1, value: "Names"}]
+        categoryList.forEach((c) => {filterOptions.push({_id: c.uid, value: c.name})})
+
+        setFilterOptions({
+                value: "Names",
+                list: filterOptions,
+                selectedList: [filterOptions[0]]
+            })
+    }, [categoryList]);
+
     const fetchData = async () => {
         try {
             const contacts = await AsyncStorage.getItem('contacts');
-            if (contacts != null)
+            if (contacts != null) {
+                // console.log(contacts)
                 setFriendList(JSON.parse(contacts))
-            else 
+            } else 
                 console.log("no entry for given key")
         } catch (error) {
             console.log("error retrieving data: " + error.message)
         }
     }
 
+    const fetchCategoryList = async () => {
+        try {
+            const value = await AsyncStorage.getItem('categories').then((value) => {
+                if(value != null){
+                    setcategoryList(JSON.parse(value));
+                }else{
+                    setcategoryList([])
+                }
+            });
+        } catch (error) {
+            console.log("error retrieving data: " + error.message)
+        }
+        return []
+    }
+
     return (
         <React.Fragment>
             <View style={styles.searchContainer}>
+
                 <SearchBar setFilterString={setFilterString}/>
+
+                {filterOptions && <PaperSelect 
+                    label = "active filter"
+                    arrayList = {[...filterOptions.list]}
+                    selectedArrayList = {filterOptions.selectedList}
+                    multiEnable = {false}
+                    value = {filterOptions.value} 
+                    onSelection = {(value) => {
+                        setFilterOptions({
+                            ...filterOptions,
+                            value: value.text,
+                            selectedList: value.selectedList,
+                            error: ''
+                        });
+
+                        value.selectedList.length > 0 ? setFilterType(value.text) : setFilterType("Names")
+                        console.log("SELECTED: " + value.text)
+                    }}
+                    modalCloseButtonText="cancel"
+                    modalDoneButtonText="select"
+                />}
+
             </View>
 
             {friendList !== undefined && friendList.length > 0 ?
                 <View>
                     <ScrollView style={styles.scrollView}>
-                        <FriendList friendList={friendList} filterString={filterString}
+                        <FriendList friendList={friendList} filterString={filterString} filterType={filterType}
                         navigation={navigation}/>
                     </ScrollView>
                 </View> :
