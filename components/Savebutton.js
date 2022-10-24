c443a1b2cc8ac06f2c036ff3a9071598553d7518
Benjamin M <lolgame99@gmail.com>
2022-10-24 17:41:38 +0200

    changed save button

--- components/Savebutton.js
+++ components/Savebutton.js
@@ -22,17 +22,13 @@ const styles = StyleSheet.create({
     navContainer: {
         width: "100%",
         justifyContent: "center",
-        alignItems: "center",
         position: "absolute",
+        alignItems: "center",
         backgroundColor: "transparent",
-        flex: 0,
-        overflow: "visible",
-        bottom: -2500
+        bottom: -2500,
     },
     fab: {
-        backgroundColor: "#80D8F7",
-        height: 60,
-        marginTop: -2640
+        marginTop: -2630,
     },
 });
 
