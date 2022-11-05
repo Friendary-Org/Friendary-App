120fde4bf9b4158ad6bc4371db72e37cb2adfbfe
Benjamin Mayer <lolgame99@gmail.com>
2022-11-05 10:02:35 +0100

    added avatars to categorytab

--- screens/CategoryTab.js
+++ screens/CategoryTab.js
@@ -4,19 +4,36 @@ import { StyleSheet, FlatList, View, Dimensions } from "react-native";
 import { Avatar, List, Text, FAB } from "react-native-paper";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { debounce } from 'lodash';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const CategoryTab = (props) => {
     const isFocused = useIsFocused();
     const { navigation } = props;
     const [categoryList, setcategoryList] = React.useState([]);
+    const [friendList, setFriendList] = React.useState([]);
+    const IMAGE = {default: require(`../assets/avatar.png`)}
 
     useEffect(() => {
         fetchCategoryList();
+        fetchFriendList();
     }, []);
 
     useEffect(() => {
         fetchCategoryList();
+        fetchFriendList();
     }, [isFocused]);
+
+    const fetchFriendList = async () => {
+        try {
+            const contacts = await AsyncStorage.getItem('contacts');
+            if (contacts != null)
+                setFriendList(JSON.parse(contacts))
+            else
+                setFriendList([])
+        } catch (error) {
+            console.log("error retrieving data: " + error.message)
+        }
+    }
 
     const fetchCategoryList = async () => {
         try {
@@ -31,17 +48,39 @@ const CategoryTab = (props) => {
     }
 
     const renderItem = (data) => {
+        let categoryFriendList = friendList.map((friend)=>{
+            let editCategory = friend.categories.find((cat) => cat.uid === data.item.uid);
+            if(editCategory !== undefined){  
+                return friend
+            }
+        })
+        let shortened = false;
+        categoryFriendList = categoryFriendList.filter(element => element !== undefined)
+        if(categoryFriendList.length > 7){
+            categoryFriendList = categoryFriendList.slice(0,7);
+            shortened = true;
+        }
         return (
             <View key={data.id} style={styles.categoryContainer}>
+            <TouchableOpacity onPress={debounce(() => navigation.push("Edit Category", { category: data.item }), 300)}>
                 <List.Item
                     title={data.item.name}
                     left={() => <Text variant={'headlineSmall'}>{data.item.icon}</Text>}
-                    onPress={debounce(() => navigation.push("Edit Category", { category: data.item }), 300)}
                 />
                 <View style={styles.avatarContainer}>
-
+                {categoryFriendList.map((friend) => {
+                    if(friend.avatar != undefined && friend.avatar.uri != undefined){
+                        return(<Avatar.Image key={friend.id} size={40} style={[{marginBottom: 10},{marginLeft: 10}]} source={{ uri: friend.avatar.uri }} />)
+                    }else if(friend.avatar != undefined){
+                        return(<Avatar.Image key={friend.id} size={40} style={[{marginBottom: 10},{marginLeft: 10}]} source={{ uri: friend.avatar }} />)
+                    }else{
+                        return(<Avatar.Image key={friend.id} size={40} style={[{marginBottom: 10},{marginLeft: 10}]} source={IMAGE["default"]} />)
+                    }                    
+                })}
+                {shortened && <Text variant="titleLarge"> ... </Text>}
                 </View>
 
+            </TouchableOpacity>
             </View>
         )
     }
