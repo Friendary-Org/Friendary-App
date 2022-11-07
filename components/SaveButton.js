efd186d93f9e563d9103a80ae9afe59c929918b4
Benjamin Mayer <lolgame99@gmail.com>
2022-11-07 12:04:24 +0100

    Create SaveButton.js

---
+++ components/SaveButton.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Dimensions } from "react-native";
+import { FAB } from "react-native-paper";
+
+const SaveButton = (props) => {
+    const {callback, disabled} = props;
+
+    return (
+            <View style={styles.navContainer}>
+                <FAB
+                    icon="content-save-outline"
+                    label="Save"
+                    style={[styles.fab]}
+                    onPress={() => callback()}
+                    size="medium"
+                    disabled={disabled != undefined?true:false}
+                />
+            </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    navContainer: {
+        width: "100%",
+        position: "absolute",
+        backgroundColor: "transparent",
+        bottom: "10%",
+        left: -800
+    },
+    fab: {
+        marginLeft: Dimensions.get("screen").width / 2 + 740,
+        width: 120
+    },
+});
+
+export default SaveButton;
